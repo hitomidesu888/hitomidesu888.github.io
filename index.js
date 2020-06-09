7f802103e732cee517dda27a8f7e557b58950ee0
hitomidesu888 <hitomiiwayama@gmail.com>
2020-06-10 06:18:44 +0900

    06100618

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-function toggleNav() {
-  var body = document.body;
-  var hamburger = document.getElementById('js-hamburger');
-  var blackBg = document.getElementById('js-black-bg');
-
-  hamburger.addEventListener('click', function() {
-    body.classList.toggle('nav-open');
-  });
-  blackBg.addEventListener('click', function() {
-    body.classList.remove('nav-open');
-  });
-}
-toggleNav();
+$(".header").css("background-color","#00f");
